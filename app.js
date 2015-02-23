485b86855ea94ef467add3efc960f6b04b72b3dc
Dan Thareja <danthareja@gmail.com>
2015-02-22 19:40:29 -0800

    (cleanup) adds comments

--- app.js
+++ app.js
@@ -1,40 +1,50 @@
-$(getBitcoinData);
+$(function() {
 
-function getBitcoinData() {
-  $.get('https://api.bitcoinaverage.com/exchanges/USD')
-    .done(displayExchanges);
-}
+  // Run every minute
+  getBitcoinData()
+  setInterval(getBitcoinData, 60000);
 
-function displayExchanges(exchanges) {
-  createNewTable();
-  $.each(exchanges, function(key, exchange) {
-    if (key === 'timestamp') return;
-    $('#exchanges').append(createTableRow(exchange))
-  });
-}
+  function getBitcoinData() {
+    $.get('https://api.bitcoinaverage.com/exchanges/USD')
+      .done(displayExchanges);
+  }
 
-function createTableRow(exchange) {
-  return '<tr>' +
-    '<td>' + exchange.display_name + '</td>' +
-    '<td>' + exchange.rates.last + '</td>' +
-    '<td>' + exchange.rates.bid + '</td>' +
-    '<td>' + exchange.rates.ask + '</td>' +
-    '<td>' + exchange.volume_btc + '</td>' +
-  '</tr>'
-}
+  // Here we a new row to our table for each exchange
+  // This function will execute after GET request is completed
+  function displayExchanges(exchanges) {
+    createNewTable();
+    $.each(exchanges, function(key, exchange) {
+      if (key === 'timestamp') return;
+      $('#exchanges').append(createTableRow(exchange))
+    });
+  }
 
-function createNewTable() {
-  var table = '<table id="exchanges">' +
-    '<colgroup span="4" class="columns"></colgroup>' +
-    '<tr>' +
-      '<th>Symbol</th>' +
-      '<th>Latest</th>' +
-      '<th>Bid</th>' +
-      '<th>Ask</th>' +
-      '<th>Volume</th>' +
-    '</tr>' +
-  '</table>';
+  // To style these, you could probably make a class for 'increased' and 'decreased'
+  function createTableRow(exchange) {
+    return '<tr>' +
+      '<td>' + exchange.display_name + '</td>' +
+      '<td>' + exchange.rates.last + '</td>' +
+      '<td>' + exchange.rates.bid + '</td>' +
+      '<td>' + exchange.rates.ask + '</td>' +
+      '<td>' + exchange.volume_btc + '</td>' +
+    '</tr>'
+  }
 
-  $('#exchanges').remove();
-  $('body').append(table)
-}
+  // Starting table with headers
+  function createNewTable() {
+    var table = '<table id="exchanges">' +
+      '<colgroup span="4" class="columns"></colgroup>' +
+      '<tr>' +
+        '<th>Symbol</th>' +
+        '<th>Latest</th>' +
+        '<th>Bid</th>' +
+        '<th>Ask</th>' +
+        '<th>Volume</th>' +
+      '</tr>' +
+    '</table>';
+
+    $('#exchanges').remove();
+    $('body').append(table)
+  }
+
+});
